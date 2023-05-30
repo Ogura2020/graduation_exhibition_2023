bb8546e81aa6fd258467dcd28dedbc5a8b6fa833
Ogura2020 <cocoanapoli@gmail.com>
2023-05-30 09:09:08 +0900

    加速度に応じて画像が変わるようにしました

--- js/rfid.js
+++ js/rfid.js
@@ -32,18 +32,22 @@ async function onConnectButtonClick() {
 }
 
 function addSerial(msg) {
-    var textarea = document.getElementById('outputArea');
-    textarea.value += msg;
-    textarea.scrollTop = textarea.scrollHeight;
+    // var textarea = document.getElementById('outputArea');
+    // textarea.value += msg;
+    // textarea.scrollTop = textarea.scrollHeight;
+
+    let kasoku = msg.split(",")
     
-    if(msg == "b2"){
+    if(msg == "b2" || kasoku[2] > 0.5){
         console.log("探偵");
         document.getElementById("img").src = 'img/chara' + chara_no[0] +'.png';
         document.getElementById("suji").innerText = "1";
-    }else if(msg == "21"){
+    }else if(msg == "21" || kasoku[1] > 0.5){
         console.log("メイド");
         document.getElementById("img").src = 'img/chara' + chara_no[1] +'.png';
         document.getElementById("suji").innerText = "2";
     }
+    //console.log(msg)
+    //console.log(kasoku[2]);
     console.log(msg)
 }
