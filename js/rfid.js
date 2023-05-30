47d5c6ecfd9cbdecf227ebf2558452a9ff8e14fa
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2023-05-30 10:29:35 +0900

    シリアルメッセージのパース

--- js/rfid.js
+++ js/rfid.js
@@ -1,14 +1,18 @@
-let port;
-
 let chara_no = new Array(1,2)
+
+const carriageReturnlRegex = new RegExp(/\r?\n|\r/, 'g');
 
 async function onConnectButtonClick() {
     try {
-        port = await navigator.serial.requestPort();
+        const port = await navigator.serial.requestPort();
         await port.open({ baudRate: 115200 });
 
         while (port.readable) {
-            const reader = port.readable.getReader();
+            const textDecoder = new TextDecoderStream();
+            const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+            const reader = textDecoder.readable
+              .pipeThrough(new TransformStream(new LineBreakTransformer()))
+              .getReader();
 
             try {
                 while (true) {
@@ -17,8 +21,8 @@ async function onConnectButtonClick() {
                         addSerial("Canceled\n");
                         break;
                     }
-                    const inputValue = new TextDecoder().decode(value);
-                    addSerial(inputValue);
+                    
+                    addSerial(value);
                 }
             } catch (error) {
                 addSerial("Error: Read" + error + "\n");
@@ -31,23 +35,31 @@ async function onConnectButtonClick() {
     }
 }
 
-function addSerial(msg) {
-    // var textarea = document.getElementById('outputArea');
-    // textarea.value += msg;
-    // textarea.scrollTop = textarea.scrollHeight;
+// /\r?\n|\r/;
 
-    let kasoku = msg.split(",")
-    
-    if(msg == "b2" || kasoku[2] > 0.5){
-        console.log("探偵");
-        document.getElementById("img").src = 'img/chara' + chara_no[0] +'.png';
-        document.getElementById("suji").innerText = "1";
-    }else if(msg == "21" || kasoku[1] > 0.5){
-        console.log("メイド");
-        document.getElementById("img").src = 'img/chara' + chara_no[1] +'.png';
-        document.getElementById("suji").innerText = "2";
+function addSerial(msg) {
+  var textarea = document.getElementById('outputArea');
+  textarea.value += msg;
+  textarea.scrollTop = textarea.scrollHeight;
+
+  // メッセージを分割する
+  const message = msg.split(',');
+
+  // 加速度のメッセージであれば数値に変換する
+  if (message.length === 4 && message[0] === 'acc') {
+    for (let i = 1; i < message.length; i++) {
+      message[i] = parseFloat(message[i], 10);
     }
-    //console.log(msg)
-    //console.log(kasoku[2]);
-    console.log(msg)
+  }
+
+  // if(msg == "b2" || kasoku[2] > 0.5){
+  //     console.log("探偵");
+  //     document.getElementById("img").src = 'img/chara' + chara_no[0] +'.png';
+  //     document.getElementById("suji").innerText = "1";
+  // }else if(msg == "21" || kasoku[1] > 0.5){
+  //     console.log("メイド");
+  //     document.getElementById("img").src = 'img/chara' + chara_no[1] +'.png';
+  //     document.getElementById("suji").innerText = "2";
+  // }
+  console.log(message);
 }
