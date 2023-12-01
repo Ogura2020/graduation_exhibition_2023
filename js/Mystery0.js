44c4afd921facf2fabd53aff1146b3a710d4634a
Ogura2020 <cocoanapoli@gmail.com>
2023-12-02 05:38:53 +0900

    音が鳴る、ヒントが表示されるようになりました

--- js/Mystery0.js
+++ js/Mystery0.js
@@ -103,6 +103,23 @@ class Dialog {
         // ダイアログ内の画像を更新
         this.dialog.querySelector('.mystery_img').src = "img/mystery0/0.png"; // ここに新しい画像のパスを設定
 
+        const nazoDialog = document.querySelector("#nazo_dialog");
+        this.tips = document.querySelector(".tips_img"); // tips 変数に値を設定
+        this.tips.src = "img/tutorial/0.png";
+
+        // ダイアログ内がクリックされたときの処理を追加
+        nazoDialog.addEventListener("click", () => {
+          // 画像の表示状態を取得
+          const show = this.tips.style.display === "block";
+
+          // 画像の表示状態を切り替える
+          if (show) {
+            this.tips.style.display = "none"; // 画像を非表示する
+          } else {
+            this.tips.style.display = "block"; // 画像を表示する
+          }
+        });
+
         // シナリオのカットのプロパティ ismystery が true ある場合
         // ダイアログを開く
         this.dialog.showModal();
@@ -124,16 +141,22 @@ class Dialog {
       this.ee.emit('select', e);
       this.imgElement = this.dialog.querySelector('.mystery_img');
 
-        // 特定のIDをチェックしてダイアログを閉じる処理
-        if (e === this.status[0].id) {
-          this.imgElement.src = "img/mystery0/" + this.imageList[0];
-        } else if (e === this.status[1].id) {
-          this.imgElement.src = "img/mystery0/" + this.imageList[1];
-        } else if (e === this.status[2].id) {
-          this.imgElement.src = "img/mystery0/" + this.imageList[2];
-        } else if (e === this.status[3].id) {
-          this.imgElement.src = "img/mystery0/" + this.imageList[3];
-        }
+      //ヒントが表示されていないとき、それぞれにあった画像に入れ替える
+      if (this.tips.style.display === "block") {
+        console.log("ヒントを表示")
+      } else if (e === this.status[0].id) {
+        this.sound.select_0.play();
+        this.imgElement.src = "img/mystery0/" + this.imageList[0];
+      } else if (e === this.status[1].id) {
+        this.sound.select_1.play();
+        this.imgElement.src = "img/mystery0/" + this.imageList[1];
+      } else if (e === this.status[2].id) {
+        this.sound.select_2.play();
+        this.imgElement.src = "img/mystery0/" + this.imageList[2];
+      } else if (e === this.status[3].id) {
+        this.sound.select_3.play();
+        this.imgElement.src = "img/mystery0/" + this.imageList[3];
+      }
     }
   }
 
@@ -149,13 +172,20 @@ class Dialog {
       let accel = e.split(',')
       console.log(accel[0],accel[1]);
 
-        // 特定のIDをチェックしてダイアログを閉じる処理
-        if (Math.abs(accel[0]) > 1.0 && this.imgElement.src.endsWith(this.imageList[0])) {
-          this.dialog.close();
-          this.dialog.classList.remove('open');
-          this.ee.emit('updateModal', { isOpen: false });
-
-        }
+      if (Math.abs(accel[0]) > 1.0 &&  this.tips.style.display === "block") {
+        console.log("ヒント表示中（block）の時は何も起こらない")
+      } else if (Math.abs(accel[0]) > 1.0 && this.imgElement.src.endsWith(this.imageList[1])) {
+        console.log("正解かつヒントが表示されていない場合、進む")
+        this.sound.clear.play();
+        this.dialog.close();
+        this.dialog.classList.remove('open');
+        this.ee.emit('updateModal', { isOpen: false });
+      } else if (Math.abs(accel[0]) > 1.0 && !this.imgElement.src.endsWith(this.imageList[1]) && this.imgElement.src.endsWith("img/mystery0/0.png")) {
+        console.log("なにも選択されてない(最初の画像)場合、振っても何も起こらない");
+      } else if (Math.abs(accel[0]) > 1.0 && !this.imgElement.src.endsWith(this.imageList[1]) ) {
+        this.sound.miss.play();
+        console.log("正解以外の場合に音が鳴る");
+      } 
     }
   }
 }
