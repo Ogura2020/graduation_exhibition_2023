d2b583cb4025ebcfa7d2545b7d5e209d3911730c
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2023-12-02 10:45:04 +0900

    画像の切り替え

--- js/Mystery0.js
+++ js/Mystery0.js
@@ -82,6 +82,26 @@ class Dialog {
             color: "green"
           }
         ]
+
+      const nazoDialog = document.querySelector('#nazo_dialog');
+      this.tips = document.querySelector('.tips_img'); // tips 変数に値を設定
+
+      // ダイアログ内がクリックされたときの処理を追加
+      nazoDialog.addEventListener('click', () => {
+        if (nazoDialog.classList.contains('mistery')) {
+          this.tips.src = 'img/tutorial/0.png';
+        }
+
+        // 画像の表示状態を取得
+        const show = this.tips.style.display === 'block';
+
+        // 画像の表示状態を切り替える
+        if (show) {
+          this.tips.style.display = 'none'; // 画像を非表示する
+        } else {
+          this.tips.style.display = 'block'; // 画像を表示する
+        }
+      });
     }
 
   /**
@@ -96,29 +116,18 @@ class Dialog {
     // ismystery が true のカットであるかの真偽値
     const nextOpenState = e.hasOwnProperty('ismystery0') && e.ismystery0;
 
+    if (nextOpenState) {
+      nazoDialog.classList.add('mistery');
+    } else {
+      nazoDialog.classList.remove('mistery');
+    }
+
     // ダイアログ要素が存在するか
     if (this.dialog) {
       if (!this.isOpen && nextOpenState) {
 
         // ダイアログ内の画像を更新
         this.dialog.querySelector('.mystery_img').src = "img/mystery0/0.png"; // ここに新しい画像のパスを設定
-
-        const nazoDialog = document.querySelector("#nazo_dialog");
-        this.tips = document.querySelector(".tips_img"); // tips 変数に値を設定
-        this.tips.src = "img/tutorial/0.png";
-
-        // ダイアログ内がクリックされたときの処理を追加
-        nazoDialog.addEventListener("click", () => {
-          // 画像の表示状態を取得
-          const show = this.tips.style.display === "block";
-
-          // 画像の表示状態を切り替える
-          if (show) {
-            this.tips.style.display = "none"; // 画像を非表示する
-          } else {
-            this.tips.style.display = "block"; // 画像を表示する
-          }
-        });
 
         // シナリオのカットのプロパティ ismystery が true ある場合
         // ダイアログを開く
